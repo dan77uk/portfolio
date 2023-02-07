69582fa2813ac549665f1a2c1ce662f076634551
Dan <dan.phillips100@gmail.com>
2023-02-07 10:58:37 +0000

    Revert "style: moved light/dark theme styling out of global CSS into components as objects"
    This reverts commit 859eff1cef4c9a014cd904fa073fd6f8ad661309.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,72 @@
 import styles from "./nav.module.css";
+import { useState } from "react";
+import { ScreenSize } from "./custom_hooks/ScreenSize";
 
-export default function Nav() {
+export default function Nav({ changeTheme, currentTheme }) {
+  const [toggleTheme, setToggleTheme] = useState("darkStyle");
+  const { width } = ScreenSize();
+
+  console.log(width);
+
+  function handleToggle() {
+    if (currentTheme === "dark") {
+      changeTheme("light");
+      setToggleTheme("lightStyle");
+    } else {
+      changeTheme("dark");
+      setToggleTheme("darkStyle");
+    }
+  }
+
+  const themeStyle = {
+    darkBack: "#000",
+  };
+
   return (
-    <header className={styles.header}>
+    <header
+      className={styles.header}
+      style={{ background: themeStyle.background }}
+    >
       <article>
-        <h1>
-          DAN <span>PHILLIPS</span>
-        </h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#home" style={{ color: themeStyle.link }}>
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="#about" style={{ color: themeStyle.link }}>
+                ABOUT
+              </a>
+            </li>
+
+            <li>
+              <a href="#contact" style={{ color: themeStyle.link }}>
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div style={{ display: "flex" }}>
+          <input
+            className={styles.themeToggle}
+            id="themeToggle"
+            type="checkbox"
+            onChange={handleToggle}
+          />
+          <label
+            className={styles.themeToggleLabel}
+            htmlFor="themeToggle"
+            style={{ backgroundColor: themeStyle.label }}
+          >
+            <span
+              className={styles.themeToggleButton}
+              style={{ backgroundColor: themeStyle.button }}
+            />
+          </label>
+        </div>
       </article>
     </header>
   );
